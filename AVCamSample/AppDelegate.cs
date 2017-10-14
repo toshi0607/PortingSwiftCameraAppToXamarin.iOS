048f676cdf49cbdc601e8364f954b62f80d6a347
toshi0607 <s.toshi0607@gmail.com>
2017-10-14 15:55:13 +0900

    add view

--- AVCamSample/AppDelegate.cs
+++ AVCamSample/AppDelegate.cs
@@ -18,8 +18,14 @@ namespace AVCamSample
 
         public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
         {
-            // Override point for customization after application launch.
-            // If not required for your application you can safely delete this method
+            Window = new UIWindow(UIScreen.MainScreen.Bounds);
+
+            var controller = new CameraViewController();
+            var navController = new UINavigationController(controller);
+
+            Window.RootViewController = navController;
+            Window.MakeKeyAndVisible();
+            Window.TintColor = UIColor.FromRGBA(1.0f, 1.0f, 0.0f, 1.0f);
 
             return true;
         }
